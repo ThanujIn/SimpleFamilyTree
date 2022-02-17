21d4805572938669e847893ffc60ee0b0c8a3f2f
ThanujIn <thanujgunasekara@gmailcom>
2022-02-17 19:05:49 +0000

    Initial commit

---
+++ src/main/java/com/example/simpleFamilyTree/SimpleFamilyTreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleFamilyTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFamilyTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFamilyTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simpleFamilyTree/SimpleFamilyTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleFamilyTree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleFamilyTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
